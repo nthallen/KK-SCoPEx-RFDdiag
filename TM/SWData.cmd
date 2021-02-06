574cc0196da38cff7392a040fb066bd2fead361e
Norton Allen <allen@huarp.harvard.edu>
2021-02-05 21:12:48 -0500

    Sequence 3

--- TM/SWData.cmd
+++ TM/SWData.cmd
@@ -18,5 +18,6 @@
   : Set %d { $0 = $2; }
   : Sequence { $0 = SWS_SEQUENCE; }
   : Sequence2 { $0 = SWS_SEQUENCE2; }
+  : Sequence3 { $0 = SWS_SEQUENCE3; }
   : Shutdown Full { $0 = SWS_SHUTDOWN; }
   ;
--- TM/SWData.h
+++ TM/SWData.h
@@ -10,6 +10,7 @@ extern SWData_t SWData;
 #define SWS_IDLE 1
 #define SWS_SEQUENCE 2
 #define SWS_SEQUENCE2 3
+#define SWS_SEQUENCE3 4
 #define SWS_SHUTDOWN 255
 
 #endif
